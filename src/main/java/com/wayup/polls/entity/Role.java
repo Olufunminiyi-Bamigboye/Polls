f9851a89fbb4d76d63e8c4771b0ef1d3cfbe8f4a
Olufunminiyi-Bamigboye <bamigboyefunminiyi07@gmail.com>
2025-02-22 15:53:20 +0000

    poll application demo

---
+++ src/main/java/com/wayup/polls/entity/Role.java
@@ -0,0 +1,62 @@
+package com.wayup.polls.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.NaturalId;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    @Column(length = 65)
+    private RoleName name;
+
+
+    public Role(RoleName name) {
+        this.name = name;
+    }
+
+    public Role() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleName getName() {
+        return name;
+    }
+
+    public void setName(RoleName name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id) && Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name=" + name +
+                '}';
+    }
+}
---
+++ src/main/java/com/wayup/polls/entity/RoleName.java
@@ -0,0 +1,6 @@
+package com.wayup.polls.entity;
+
+public enum RoleName {
+    ROLE_ADMIN,
+    ROLE_USER
+}
